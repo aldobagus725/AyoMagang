fbd4ad7cbda474d2f3a5b63558b461d6be196e81
aldobagus725 <50010046+aldobagus725@users.noreply.github.com>
2020-07-26 10:47:52 +0800

    Ayo magang alpha 26-07-2020

---
+++ ayomagang/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+        <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="asset/css/bootstrap-grid.min.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link href="starter-template.css" rel="stylesheet">
+		<title>Ayo Magang</title>
+	</head>
+
+	<body>
+		<?php  
+			include 'navbar.php';
+		?>
+		<div class="container">
+			<div class="starter-template">
+				<h1>Bootstrap starter template</h1>
+				<p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+			</div>
+		</div>
+	</body>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+		<script src="../../../../assets/js/ie10-viewport-bug-workaround.js"></script>
+        <script src="asset/js/jquery-3.4.1.min.js" type="text/javascript"></script>
+        <script src="asset/js/bootstrap.js" type="text/javascript"></script>
+        <script src="asset/js/magnific-popup.js" type="text/javascript"></script>
+        <script src="asset/js/jquery.interactive_bg.js" type="text/javascript"></script>
+</html>

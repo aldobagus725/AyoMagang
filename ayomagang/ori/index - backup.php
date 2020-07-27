2b7d3c6652e1cb8c6b6301a93eeb02ef384cc536
aldobagus725 <50010046+aldobagus725@users.noreply.github.com>
2020-07-28 02:02:23 +0800

    Update Homepage + SuperAdmin

--- ayomagang/ori/index - backup.php
+++ ayomagang/ori/index - backup.php
@@ -31,6 +31,11 @@
 			            <li class="nav-item">
 			                <a class="nav-link scroll-link" href="#top-content">Beranda</a>
 			            </li>
+<!--
+			            <li class="nav-item">
+			                <a class="nav-link scroll-link" href="#services">Info Magang</a>
+			            </li>
+-->
 			            <li class="nav-item">
 			                <a class="nav-link scroll-link" href="#about-us">Tentang Kami</a>
 			            </li>
@@ -40,11 +45,11 @@
 			            <li class="nav-item">
 			                <a class="nav-link scroll-link" href="#testimonials">Testimoni</a>
 			            </li>
-                        <li class="nav-item">
-			                <a class="nav-link" href="login.php">Tertarik? Ayo Login</a>
+			            <li class="nav-item">
+			                <a class="nav-link scroll-link" href="#blog">blog</a>
 			            </li>
                         <li class="nav-item">
-			                <a class="nav-link" href="/ayomagang/superadmin/login.php">Admin Login Sini</a>
+			                <a class="nav-link" href="login.php">Login</a>
 			            </li>
 			        </ul>
 			    </div>
@@ -55,8 +60,10 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 offset-md-2 text">
+<!--                        <h1 class="wow fadeInLeftBig">Ayo Magang: Getting Academic Internship Easier</h1>-->
                         <h1 class="wow fadeInLeftBig">Mencari Magang Akademik Menjadi Lebih Mudah!</h1>
                         <div class="description wow fadeInLeftBig">
+<!--                        	<p>Helps you out to get the desired internship for your academic needs!</p>-->
                             <p>Membantu kamu mencari magang akademik sesuai kebutuhanmu!</p>
                         </div>
                         <div class="top-big-link wow fadeInUp">
@@ -64,6 +71,13 @@
                             <br/>
                             <button class="btn btn-success" type="submit">Cari</button>
                         </div>
+<!--
+                        <div class="top-big-link wow fadeInUp">
+                        	<input class="form-control mr-sm-2" type="text" placeholder="Course name, field of study, i.e. Information System">
+                            <br/>
+                            <button class="btn btn-success" type="submit">Search</button>
+                        </div>
+-->
                     </div>
                 </div>
             </div>            
@@ -152,6 +166,93 @@
 	            </div>
 	        </div>
         </div>
+
+		<!-- More services -->
+<!--
+        <div class="more-services-container section-container">
+	        <div class="container">
+	        	
+	            <div class="row">
+	                <div class="col more-services section-description wow fadeIn">
+	                    <h2>More Services</h2>
+	                    <div class="divider-1 wow fadeInUp"><span></span></div>
+	                </div>
+	            </div>
+	            
+	            <div class="row">
+	                <div class="col-md-6 more-services-box wow fadeInLeft">
+	                	<div class="row">
+	                		<div class="col-md-3">
+	                			<div class="more-services-box-icon">
+	                				<i class="fab fa-twitter"></i>
+	                			</div>
+	                		</div>
+	                		<div class="col-md-9">
+	                			<h3>Ut wisi enim ad minim</h3>
+		                    	<p>
+		                    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
+		                    		Ut wisi enim ad minim veniam, quis nostrud.
+		                    	</p>
+	                		</div>
+	                	</div>
+	                </div>
+	                <div class="col-md-6 more-services-box wow fadeInLeft">
+	                	<div class="row">
+	                		<div class="col-md-3">
+	                			<div class="more-services-box-icon">
+	                				<i class="fas fa-pencil-alt"></i>
+	                			</div>
+	                		</div>
+	                		<div class="col-md-9">
+	                			<h3>Sed do eiusmod tempor</h3>
+		                    	<p>
+		                    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
+		                    		Ut wisi enim ad minim veniam, quis nostrud.
+		                    	</p>
+	                		</div>
+	                	</div>
+	                </div>
+	            </div>
+	            
+	            <div class="row">
+	                <div class="col-md-6 more-services-box wow fadeInLeft">
+	                	<div class="row">
+	                		<div class="col-md-3">
+	                			<div class="more-services-box-icon">
+	                				<i class="fas fa-cloud"></i>
+	                			</div>
+	                		</div>
+	                		<div class="col-md-9">
+	                			<h3>Quis nostrud exerci tat</h3>
+		                    	<p>
+		                    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
+		                    		Ut wisi enim ad minim veniam, quis nostrud.
+		                    	</p>
+	                		</div>
+	                	</div>
+	                </div>
+	                <div class="col-md-6 more-services-box wow fadeInLeft">
+	                	<div class="row">
+	                		<div class="col-md-3">
+	                			<div class="more-services-box-icon">
+	                				<i class="fab fa-pinterest"></i>
+	                			</div>
+	                		</div>
+	                		<div class="col-md-9">
+	                			<h3>Minim veniam quis nostrud</h3>
+		                    	<p>
+		                    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
+		                    		Ut wisi enim ad minim veniam, quis nostrud.
+		                    	</p>
+	                		</div>
+	                	</div>
+	                </div>
+	            </div>
+
+	        </div>
+        </div>
+-->
+        
         <!-- Portfolio -->
         <div class="portfolio-container section-container">
 	        <div class="container">
@@ -289,24 +390,118 @@
         		</div>
         	</div>
         </div>
+                
+        <!-- Blog -->
+<!--
+        <div class="blog-container section-container">
+	        <div class="container">
+	            <div class="row">
+	                <div class="col blog section-description wow fadeIn">
+	                    <h2>The Blog</h2>
+	                    <div class="divider-1 wow fadeInUp"><span></span></div>
+	                </div>
+	            </div>
+	            <div class="row">
+	                <div class="col-md-6 blog-box wow fadeInLeft">
+	                	<div class="row">
+	                		<div class="col-md-3">
+	                			<div class="blog-box-image">
+	                				<img src="assets/img/blog/1.jpg" alt="" data-at2x="assets/img/blog/1.jpg">
+	                			</div>
+	                		</div>
+	                		<div class="col-md-9">
+	                			<h3><a href="#">Ut wisi enim ad minim</a> <i class="fas fa-angle-right"></i></h3>
+	                			<div class="blog-box-date"><i class="far fa-calendar"></i> 17 September 2018</div>
+		                    	<p>
+		                    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
+		                    	</p>
+	                		</div>
+	                	</div>
+	                </div>
+	                <div class="col-md-6 blog-box wow fadeInLeft">
+	                	<div class="row">
+	                		<div class="col-md-3">
+	                			<div class="blog-box-image">
+	                				<img src="assets/img/blog/2.jpg" alt="" data-at2x="assets/img/blog/2.jpg">
+	                			</div>
+	                		</div>
+	                		<div class="col-md-9">
+	                			<h3><a href="#">Sed do eiusmod tempor</a> <i class="fas fa-angle-right"></i></h3>
+	                			<div class="blog-box-date"><i class="far fa-calendar"></i> 13 August 2018</div>
+		                    	<p>
+		                    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
+		                    	</p>
+	                		</div>
+	                	</div>
+	                </div>
+	            </div>
+	            
+	            <div class="row">
+	                <div class="col-md-6 blog-box wow fadeInLeft">
+	                	<div class="row">
+	                		<div class="col-md-3">
+	                			<div class="blog-box-image">
+	                				<img src="assets/img/blog/3.jpg" alt="" data-at2x="assets/img/blog/3.jpg">
+	                			</div>
+	                		</div>
+	                		<div class="col-md-9">
+	                			<h3><a href="#">Quis nostrud exerci tat</a> <i class="fas fa-angle-right"></i></h3>
+	                			<div class="blog-box-date"><i class="far fa-calendar"></i> 26 July 2018</div>
+		                    	<p>
+		                    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
+		                    	</p>
+	                		</div>
+	                	</div>
+	                </div>
+	                <div class="col-md-6 blog-box wow fadeInLeft">
+	                	<div class="row">
+	                		<div class="col-md-3">
+	                			<div class="blog-box-image">
+	                				<img src="assets/img/blog/4.jpg" alt="" data-at2x="assets/img/blog/4.jpg">
+	                			</div>
+	                		</div>
+	                		<div class="col-md-9">
+	                			<h3><a href="#">Minim veniam quis nostrud</a> <i class="fas fa-angle-right"></i></h3>
+	                			<div class="blog-box-date"><i class="far fa-calendar"></i> 09 June 2018</div>
+		                    	<p>
+		                    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
+		                    	</p>
+	                		</div>
+	                	</div>
+	                </div>
+	            </div>
+	            <div class="row">
+	            	<div class="col section-bottom-button wow fadeInUp">
+                        <a class="btn btn-primary btn-link-3" href="#">Visit the blog</a>
+	            	</div>
+	            </div>
+
+	        </div>
+        </div>
+-->
+
         <!-- Footer -->
         <footer>
 	        <div class="container">
-	        	<div class="row"> 
-                    <div class="col-sm-3 offset-md-1 footer-bottom"></div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-3 offset-md-1 footer-bottom"></div>
-                </div>
-                <div class="row">
-                    <div class="col offset-md-1 footer-bottom">
+	        	<div class="row">
+<!--
+                    <div class="col-md-5 footer-left">
+                    	<p>&copy; Bootstrap 4 One Page Website Tutorial. Download it for free on <a href="http://azmind.com">AZMIND</a>.</p>
+                    </div>
+-->
+                    
+                    <div class="col-md-6 offset-md-1 footer-right">
                     	<a href="#"><i class="fab fa-facebook-f"></i></a>
+						<a href="#"><i class="fab fa-dribbble"></i></a>
 						<a href="#"><i class="fab fa-twitter"></i></a>
 						<a href="#"><i class="fab fa-instagram"></i></a>
 						<a href="#"><i class="fab fa-pinterest"></i></a>
-                        <br>
-                        <a class="scroll-link" href="#top-content"><i class="fas fa-chevron-up"></i></a>
                     </div>
+                </div>
+                <div class="row">
+                	<div class="col footer-bottom">
+		           		<a class="scroll-link" href="#top-content"><i class="fas fa-chevron-up"></i></a>
+		           	</div>
                 </div>
 	        </div>
         </footer>

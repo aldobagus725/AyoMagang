912d8969e184b1670e528cffe0bd144ec9113e84
aldobagus725 <50010046+aldobagus725@users.noreply.github.com>
2020-07-28 13:38:07 +0800

    Push baru alpha iteration
    Push baru alpha iteration

---
+++ ayomagang/index.php
@@ -0,0 +1,322 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Welcome!</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,600">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:300">
+		<link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css">
+        <link rel="stylesheet" href="assets/css/animate.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/media-queries.css">
+        <link rel="shortcut icon" href="assets/ico/favicon.png">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+    </head>
+    <body>
+		<!-- NavBar -->
+		<nav class="navbar navbar-dark fixed-top navbar-expand-md navbar-no-bg">
+			<div class="container">
+				<a class="navbar-brand" href="index.php">Ayo Magang</a>
+			    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			        <span class="navbar-toggler-icon"></span>
+			    </button>
+			    <div class="collapse navbar-collapse" id="navbarNav">
+			        <ul class="navbar-nav ml-auto">
+			            <li class="nav-item">
+			                <a class="nav-link scroll-link" href="#top-content">Beranda</a>
+			            </li>
+			            <li class="nav-item">
+			                <a class="nav-link scroll-link" href="#about-us">Tentang Kami</a>
+			            </li>
+			            <li class="nav-item">
+			                <a class="nav-link scroll-link" href="#portfolio">Kerjasama</a>
+			            </li>
+			            <li class="nav-item">
+			                <a class="nav-link scroll-link" href="#testimonials">Testimoni</a>
+			            </li>
+                        <li class="nav-item">
+			                <a class="nav-link" href="login.php">Tertarik? Ayo Login</a>
+			            </li>
+                        <li class="nav-item">
+			                <a class="nav-link" href="/ayomagang/superadmin/login.php">Admin Login Sini</a>
+			            </li>
+			        </ul>
+			    </div>
+		    </div>
+		</nav>
+        <!-- Home -->
+        <div class="top-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 offset-md-2 text">
+                        <h1 class="wow fadeInLeftBig">Mencari Magang Akademik Menjadi Lebih Mudah!</h1>
+                        <div class="description wow fadeInLeftBig">
+                            <p>Membantu kamu mencari magang akademik sesuai kebutuhanmu!</p>
+                        </div>
+                        <div class="top-big-link wow fadeInUp">
+                        	<input class="form-control mr-sm-2" type="text" placeholder="Nama Jurusan, Bidang Studi, contoh : Sistem Informasi">
+                            <br/>
+                            <button class="btn btn-success" type="submit">Cari</button>
+                        </div>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        
+        <!-- Services -->
+        <div class="services-container section-container">
+	        <div class="container">
+	            <div class="row">
+	                <div class="col services section-description wow fadeIn">
+	                    <h2 style="color: white;">Kenapa Ayo Magang?</h2>
+	                    <div class="divider-1 wow fadeInUp"><span></span></div>
+	                </div>
+	            </div>
+	            <div class="row">
+                	<div class="col-md-4 services-box wow fadeInUp">
+                		<div class="row">
+                			<div class="col-md-4">
+			                	<div class="services-box-icon">
+			                		<i class="fas fa-magic"></i>
+			                	</div>
+		                	</div>
+	                		<div class="col-md-8">
+	                    		<h3 style="color: white">Banyak Pilihan</h3>
+	                    		<p style="color: white;">Banyak pilihan perusahaan yang siap menerima kamu untuk magang!</p>
+	                    	</div>
+	                    </div>
+                    </div>
+                    <div class="col-md-4 services-box wow fadeInDown">
+	                	<div class="row">
+                			<div class="col-md-4">
+			                	<div class="services-box-icon">
+			                		<i class="fas fa-cog"></i>
+			                	</div>
+		                	</div>
+	                		<div class="col-md-8">
+	                    		<h3 style="color: white;">Kustomisasi bebas</h3>
+	                    		<p style="color: white;">Bebas untuk mengatur pencarian sesuai kebutuhanmu! Sistem kita juga membantu menyediakan preferensi sesuai profil kamu!</p>
+	                    	</div>
+	                    </div>
+                    </div>
+                    <div class="col-md-4 services-box wow fadeInUp">
+	                	<div class="row">
+                			<div class="col-md-4">
+			                	<div class="services-box-icon">
+			                		<i class="fab fa-google"></i>
+			                	</div>
+		                	</div>
+	                		<div class="col-md-8">
+	                    		<h3 style="color: white;">Marketing</h3>
+	                    		<p style="color: white;">Buat anda yang mencari anak magang, selain jadi recruiter, anda juga bisa promosi dan ekspose lebih luas terhadap brand anda!</p>
+	                    	</div>
+	                    </div>
+                    </div>
+	            </div>
+	        </div>
+        </div>
+
+        <!-- About us -->
+        <div class="about-us-container section-container section-container-gray-bg">
+	        <div class="container">
+	            <div class="row">
+	            	<div class="col-12 col-lg-7 about-us-box wow fadeInLeft">
+	                    <div class="about-us-box-text">
+	                    	<h3>Tentang Kami</h3>
+	                    	<p class="medium-paragraph">
+	                    		Ayo Magang adalah aplikasi web yang membantu kamu siswa atau mahasiswa praktek
+                                dalam mencari perusahaan sebagai tempat magang akademik kalian!
+	                    	</p>
+	                    	<p>
+	                    		Aplikasi web kami sudah dilengkapi dengan teknologi terbaru, menggunakan
+                                properti web yang ringan, sehingga kalian bisa dengan cepat mengakses informasi
+                                yang kalian butuhkan!
+	                    	</p>
+	                    	<p>
+	                    		Kamu juga sudah bekerja sama dengan banyak perusahaan, sehingga kalian memiliki
+                                banyak pilihan dalam mencari perusahaan sesuai jurusan ataupun preferensi kalian.
+	                    	</p>
+	                    </div>
+	                </div>
+	                <div class="col-12 col-lg-5 about-us-box wow fadeInUp">
+	                	<div class="about-us-box-img">
+	                    	<img src="assets/img/about/1.jpg" alt="about" data-at2x="assets/img/about/1.jpg">
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+        </div>
+        <!-- Portfolio -->
+        <div class="portfolio-container section-container">
+	        <div class="container">
+	            <div class="row">
+	                <div class="col portfolio section-description wow fadeIn">
+	                    <h2>Kerjasama kami</h2>
+	                    <div class="divider-1 wow fadeInUp"><span></span></div>
+	                    <p>Kita telah bekerja sama dengan lebih dari 50 perusahaan yang siap menerima kalian sesuai jurusan kalian</p>
+	                </div>
+	            </div>
+	            <div class="row">
+                	<div class="col-md-4 portfolio-box wow fadeInUp">
+	                	<div class="portfolio-box-image">
+	                		<img src="assets/img/portfolio/1.jpg" alt="" data-at2x="assets/img/portfolio/1.jpg">
+	                	</div>
+                		<h3><a href="#">Perusahaan A</a> <i class="fas fa-angle-right"></i></h3>
+                		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+                    </div>
+                    <div class="col-md-4 portfolio-box wow fadeInDown">
+	                	<div class="portfolio-box-image">
+	                		<img src="assets/img/portfolio/2.jpg" alt="" data-at2x="assets/img/portfolio/2.jpg">
+	                	</div>
+                		<h3><a href="#">Perusahaan B</a> <i class="fas fa-angle-right"></i></h3>
+                		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+                    </div>
+                    <div class="col-md-4 portfolio-box wow fadeInUp">
+	                	<div class="portfolio-box-image">
+	                		<img src="assets/img/portfolio/3.jpg" alt="" data-at2x="assets/img/portfolio/3.jpg">
+	                	</div>
+                		<h3><a href="#">Perusahaan C</a> <i class="fas fa-angle-right"></i></h3>
+                		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+                    </div>
+                    <div class="col-md-4 portfolio-box wow fadeInUp">
+	                	<div class="portfolio-box-image">
+	                		<img src="assets/img/portfolio/3.jpg" alt="" data-at2x="assets/img/portfolio/3.jpg">
+	                	</div>
+                		<h3><a href="#">Perusahaan D</a> <i class="fas fa-angle-right"></i></h3>
+                		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+                    </div>
+                    <div class="col-md-4 portfolio-box wow fadeInUp">
+	                	<div class="portfolio-box-image">
+	                		<img src="assets/img/portfolio/3.jpg" alt="" data-at2x="assets/img/portfolio/3.jpg">
+	                	</div>
+                		<h3><a href="#">Perusahaan E</a> <i class="fas fa-angle-right"></i></h3>
+                		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+                    </div>
+                    <div class="col-md-4 portfolio-box wow fadeInUp">
+	                	<div class="portfolio-box-image">
+	                		<img src="assets/img/portfolio/3.jpg" alt="" data-at2x="assets/img/portfolio/3.jpg">
+	                	</div>
+                		<h3><a href="#">Perusahaan F</a> <i class="fas fa-angle-right"></i></h3>
+                		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+                    </div>
+	            </div>
+	            <div class="row">
+	            	<div class="col section-bottom-button wow fadeInUp">
+                        <a class="btn btn-primary btn-link-3" href="#">Muat Lebih Banyak</a>
+	            	</div>
+	            </div>
+	        </div>
+        </div>
+        <!-- Testimonials -->
+        <div class="testimonials-container section-container section-container-image-bg">
+        	<div class="container">
+        		<div class="row">
+        			<div class="col testimonials section-description wow fadeIn">
+                        <h2 style="color: white;">Testimoni</h2>
+                    </div>
+        		</div>
+        		<div class="row">
+        			<div class="col-md-10 offset-md-1 testimonial-list wow fadeInUp">
+        				<div class="tab-content" id="myTabContent">
+        					<div class="tab-pane fade show active" id="tab1" role="tabpanel" aria-labelledby="tab1-tab">
+        						<div class="testimonial-image">
+                					<img src="assets/img/testimonials/1.jpg" alt="testimonial-1" data-at2x="assets/img/testimonials/1.jpg">
+                				</div>
+                				<div class="testimonial-text">
+	                                <p>
+	                                	"Ayo Magang Keren Banget"<br>
+	                                	<a href="#">- John Doe</a>
+	                                </p>
+                                </div>
+        					</div>
+        					<div class="tab-pane fade" id="tab2" role="tabpanel" aria-labelledby="tab2-tab">
+        						<div class="testimonial-image">
+                					<img src="assets/img/testimonials/2.jpg" alt="testimonial-2" data-at2x="assets/img/testimonials/2.jpg">
+                				</div>
+                				<div class="testimonial-text">
+	                                <p>
+	                                	"Ayo Magang Keren Banget"<br>
+	                                	<a href="#">- John Doe</a>
+	                                </p>
+                                </div>
+        					</div>
+        					<div class="tab-pane fade" id="tab3" role="tabpanel" aria-labelledby="tab3-tab">
+        						<div class="testimonial-image">
+                					<img src="assets/img/testimonials/3.jpg" alt="testimonial-3" data-at2x="assets/img/testimonials/3.jpg">
+                				</div>
+                				<div class="testimonial-text">
+	                                <p>
+	                                	"Ayo Magang Keren Banget"<br>
+	                                	<a href="#">- John Doe</a>
+	                                </p>
+                                </div>
+							</div>
+        					<div class="tab-pane fade" id="tab4" role="tabpanel" aria-labelledby="tab4-tab">
+        						<div class="testimonial-image">
+                					<img src="assets/img/testimonials/4.jpg" alt="" data-at2x="assets/img/testimonials/4.jpg">
+                				</div>
+                				<div class="testimonial-text">
+	                                <p>
+	                                	"Ayo Magang Keren Banget"<br>
+	                                	<a href="#">- John Doe</a>
+	                                </p>
+                                </div>
+        					</div>
+        				</div>
+        				<ul class="nav nav-tabs" id="myTab" role="tablist">
+        					<li class="nav-item">
+        						<a class="nav-link active" id="tab1-tab" data-toggle="tab" href="#tab1" role="tab" aria-controls="tab1" aria-selected="true"></a>
+        					</li>
+        					<li class="nav-item">
+        						<a class="nav-link" id="tab2-tab" data-toggle="tab" href="#tab2" role="tab" aria-controls="tab2" aria-selected="false"></a>
+        					</li>
+        					<li class="nav-item">
+        						<a class="nav-link" id="tab3-tab" data-toggle="tab" href="#tab3" role="tab" aria-controls="tab3" aria-selected="false"></a>
+        					</li>
+        					<li class="nav-item">
+        						<a class="nav-link" id="tab4-tab" data-toggle="tab" href="#tab4" role="tab" aria-controls="tab4" aria-selected="false"></a>
+        					</li>
+        				</ul>
+        			</div>
+        		</div>
+        	</div>
+        </div>
+        <!-- Footer -->
+        <footer>
+	        <div class="container">
+	        	<div class="row"> 
+                    <div class="col-sm-3 offset-md-1 footer-bottom"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-3 offset-md-1 footer-bottom"></div>
+                </div>
+                <div class="row">
+                    <div class="col offset-md-1 footer-bottom">
+                    	<a href="#"><i class="fab fa-facebook-f"></i></a>
+						<a href="#"><i class="fab fa-twitter"></i></a>
+						<a href="#"><i class="fab fa-instagram"></i></a>
+						<a href="#"><i class="fab fa-pinterest"></i></a>
+                        <br>
+                        <a class="scroll-link" href="#top-content"><i class="fas fa-chevron-up"></i></a>
+                    </div>
+                </div>
+	        </div>
+        </footer>
+        <!-- Javascript -->
+        <script src="assets/js/jquery-3.2.1.min.js"></script>
+        <script src="assets/js/jquery-migrate-3.0.0.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="assets/js/jquery.backstretch.min.js"></script>
+        <script src="assets/js/wow.min.js"></script>
+        <script src="assets/js/retina-1.1.0.min.js"></script>
+        <script src="assets/js/waypoints.min.js"></script>
+        <script src="assets/js/scripts.js"></script>
+    </body>
+</html>
--- ayomagang/login.php
+++ ayomagang/login.php
@@ -1,5 +1,5 @@
-<?php
-	session_start();
+<?php 
+    session_start();
     include 'koneksi.php';
 ?>
 <!DOCTYPE html>
@@ -8,66 +8,114 @@
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<title>Ayo Magang | Login</title>
+		<title>Ayo Magang</title>
 
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/login.css">
 	</head>
 	<body>
-		<?php include 'menu.php'; ?>
-		<br>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4 offset-md-4">
-					<div class="card">
-						<div class="card-header">
-							<h3 class="card-title">Login</h3>
-						</div>
-						<div class="card-body">
-							<form method="POST" class="form-horizontal">
-								<div class="form-group">
-									<label class="control-label">ID</label>
-									<input type="text" class="form-control" name="username" required="" placeholder="Masukkan Email or Username...">
-								</div>
-								<div class="form-group">
-									<label class="control-label">Password</label>
-									<input type="password" class="form-control" name="password" required="" placeholder="Masukkan Password...">
-								</div>
-								<div class="form-group">
-									<button class="btn btn-primary" name="login">Login</button>
-								</div>
-							</form>
-							<?php
-								if(isset($_POST['login'])){
-									$username = $_POST['username'];
-									$password = $_POST['password'];
-									//Mengambil data email dan password pada tabel "company"
-									//Login berdasarkan username dan email
-									$query = $koneksi->query("SELECT * FROM company WHERE 
-										(email    = '$username' OR username = '$username' ) AND 
-										 password = '$password'");
-									//Menghitung data(akun)
-									$data = $query->num_rows;
-									//Jika akun ada yang cocok
-									if($data == 1){
-										$akun = $query->fetch_assoc(); 
-										$_SESSION['company'] = $akun;
-										echo "<div class='alert alert-info'>Login Successful!</div>";
-										echo "<script>location='index.php';</script>";
-									}
-									//Jika akun tidak ada yang cocok
-									else{
-										echo "<div class='alert alert-danger'>You failed to login, please try again!</div>";
-										echo "<meta http-equiv='refresh' content='1; url=login.php'>"; 
-									}
-								}
-							?>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<br>
+		<?php 
+			include 'navbar.php';
+		?>
+		<div class="container login-container">
+            <div class="row">
+                <div class="col-md-6 login-form-1">
+                    <h3>Student</h3>
+                    <form method="POST">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="username" placeholder="Your Email or Username *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" name="password" placeholder="Your Password *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btnSubmit" value="Login" name="logstudent"> 
+                        </div>
+                        <div class="form-group">
+                            <a href="#" class="ForgetPwd">Forget Password?</a>
+                        </div>
+                        <div class="form-group">
+                            <p>Ingin menjadi sahabat magang ?</p>
+                            <p>Klik di <a href="student/register-student.php" class="" style="text-decoration: none;"><b>Sini</b></a><p>
+                        </div>
+                    </form>
+                             <?php
+                                if(isset($_POST['logstudent'])){
+                                    $username = $_POST['username'];
+                                    $password = $_POST['password'];
+                                    //Mengambil data email_pelanggan dan password_pelanggan pada tabel "pelanggan"
+                                    //Login berdasarkan username dan email
+                                    $query = $koneksi->query("SELECT * FROM student WHERE 
+                                        (email    = '$username' OR username = '$username' ) AND 
+                                         password = '$password'");
+                                    //Menghitung data(akun)
+                                    $data = $query->num_rows;
+                                    //Jika akun ada yang cocok
+                                    if($data == 1){
+                                        $akun = $query->fetch_assoc(); 
+                                        $_SESSION['student'] = $akun;
+                                        echo "<div class='alert alert-info'>Login Berhasil!</div>";
+                                        echo "<script>location='student/home.php';</script>";
+                                    }
+                                    //Jika akun tidak ada yang cocok
+                                    else{
+                                        echo "<div class='alert alert-danger'>Login Gagal!</div>";
+                                        echo "<meta http-equiv='refresh' content='1; url=login.php'>"; 
+                                    }
+                                }
+                            ?>                  
+                </div>
+                <div class="col-md-6 login-form-2">
+                    <h3>Company</h3>
+                    <form method="POST">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="username" placeholder="Your Email or Username *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" name="password" placeholder="Your Password *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btnSubmit" value="Login" name="logcompany">
+                        </div>
+                        <div class="form-group">
+                            <a href="#" class="ForgetPwd" value="Login">Forget Password?</a>
+                        </div>
+                        <div class="form-group">
+                            <p style="color: white;">Ingin mencari pemagang ?</p>
+                            <p style="color: white;">Klik di <a href="company/register-company.php" class="" style="text-decoration: none; color: black;"><b>Sini</b></a><p>
+                        </div>
+                    </form>
+                            <?php
+                                if(isset($_POST['logcompany'])){
+                                    $username = $_POST['username'];
+                                    $password = $_POST['password'];
+                                    //Mengambil data email_pelanggan dan password_pelanggan pada tabel "pelanggan"
+                                    //Login berdasarkan username dan email
+                                    $query = $koneksi->query("SELECT * FROM company WHERE 
+                                        (email    = '$username' OR username = '$username' ) AND 
+                                         password = '$password'");
+                                    //Menghitung data(akun)
+                                    $data = $query->num_rows;
+                                    //Jika akun ada yang cocok
+                                    if($data == 1){
+                                        $akun = $query->fetch_assoc(); 
+                                        $_SESSION['company'] = $akun;
+                                        echo "<div class='alert alert-info'>Login Berhasil!</div>";
+                                        echo "<script>location='company/home.php';</script>";
+                                    }
+                                    //Jika akun tidak ada yang cocok
+                                    else{
+                                        echo "<div class='alert alert-danger'>Login Gagal!</div>";
+                                        echo "<meta http-equiv='refresh' content='1; url=login.php'>"; 
+                                    }
+                                }
+                            ?>                      
+                </div>
+            </div>
+        </div>
+
 		<!-- Optional JavaScript -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>

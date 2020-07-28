f74b5c0de6ca3309aea224d0a2d1b7ced6d3c87e
LuhWulandari <60771629+LuhWulandari@users.noreply.github.com>
2020-07-28 12:01:29 +0800

    New detail company

--- ayomagang/ayomagang (1).sql
+++ ayomagang/ayomagang (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 25, 2020 at 02:59 PM
+-- Generation Time: Jul 27, 2020 at 09:08 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.2.32
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `company` (
-  `id` int(11) NOT NULL,
+  `id_perusahaan` int(11) NOT NULL,
   `nama_perusahaan` varchar(100) NOT NULL,
   `username` varchar(100) NOT NULL,
   `siup` int(11) NOT NULL,
@@ -42,8 +42,37 @@ CREATE TABLE `company` (
 -- Dumping data for table `company`
 --
 
-INSERT INTO `company` (`id`, `nama_perusahaan`, `username`, `siup`, `alamat`, `telepon`, `email`, `password`) VALUES
-(1, 'Margarin', 'margarin', 123, '123', '123', 'margarin@gmail.com', 'margarin');
+INSERT INTO `company` (`id_perusahaan`, `nama_perusahaan`, `username`, `siup`, `alamat`, `telepon`, `email`, `password`) VALUES
+(3, 'admin', 'admin', 123, 'jl.admin', '123', 'admin@gmail.com', 'admin'),
+(4, 'test', 'test', 456, 'jl.test', '456', 'test@gmail.com', 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `open_app`
+--
+
+CREATE TABLE `open_app` (
+  `id_openapp` int(11) NOT NULL,
+  `id_perusahaan` int(11) NOT NULL,
+  `nama_perusahaan` varchar(100) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `alamat_perusahaan` varchar(100) NOT NULL,
+  `telepon` varchar(100) NOT NULL,
+  `bidang_perusahaan` varchar(100) NOT NULL,
+  `syarat_magang` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `open_app`
+--
+
+INSERT INTO `open_app` (`id_openapp`, `id_perusahaan`, `nama_perusahaan`, `author`, `alamat_perusahaan`, `telepon`, `bidang_perusahaan`, `syarat_magang`) VALUES
+(7, 4, 'test', 'lesti test', 'jl.test', '456', 'website', 'bisa buat web\r\n'),
+(8, 3, 'admin', 'admin', 'jl.admin', '123', 'web', 'web\r\n'),
+(10, 3, 'admin', 'admin2', 'jl.admin', '123', 'admin2', 'admin2'),
+(11, 4, 'test', 'test2', 'jl.test', '456', 'test2', 'test2'),
+(12, 4, 'test', 'test3', 'jl.test', '456', 'test3', 'test3');
 
 -- --------------------------------------------------------
 
@@ -79,7 +108,13 @@ INSERT INTO `student` (`id`, `nama_lengkap`, `username`, `nis`, `alamat`, `telep
 -- Indexes for table `company`
 --
 ALTER TABLE `company`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_perusahaan`);
+
+--
+-- Indexes for table `open_app`
+--
+ALTER TABLE `open_app`
+  ADD PRIMARY KEY (`id_openapp`);
 
 --
 -- Indexes for table `student`
@@ -95,7 +130,13 @@ ALTER TABLE `student`
 -- AUTO_INCREMENT for table `company`
 --
 ALTER TABLE `company`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_perusahaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `open_app`
+--
+ALTER TABLE `open_app`
+  MODIFY `id_openapp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `student`

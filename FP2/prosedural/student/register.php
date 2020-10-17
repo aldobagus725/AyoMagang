4d240f009fd3435d3fe345ece4c56c28dfb2297b
aldobagus725 <50010046+aldobagus725@users.noreply.github.com>
2020-10-17 14:26:00 +0800

    perbaiki koneksi.php

--- FP2/prosedural/student/register.php
+++ FP2/prosedural/student/register.php
@@ -1,6 +1,6 @@
 <?php 
     session_start();
-    include 'koneksi.php';
+    include '../koneksi.php';
     $spesialisasi = array("Perhotelan", "Kelistrikan", "Teknologi", "Kuliner", "Jurnalis", "Akuntansi", "Marketing",);
     sort($spesialisasi);
 ?>

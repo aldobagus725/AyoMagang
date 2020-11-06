05e2a8f65e194f1850ee936f9efb66e83c9a593f
aldobagus725 <50010046+aldobagus725@users.noreply.github.com>
2020-11-07 00:48:25 +0800

    fix resetPassword

--- FP2/oop mantap/student/resetPassword.php
+++ FP2/oop mantap/student/resetPassword.php
@@ -78,13 +78,13 @@
                                 <div class="input-group-prepend">
                                     <span class="input-group-text">Password Baru</span>
                                 </div>
-                                <input type="new_password" class="form-control" name="password" required autofocus placeholder="Masukkan Password baru anda disini">
+                                <input type="password" class="form-control" name="new_password" required autofocus placeholder="Masukkan Password baru anda disini">
                             </div>
                             <div class="input-group mb-3">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text">Confirm Password Baru</span>
                                 </div>
-                                <input type="confirm_new_password" class="form-control" name="password" required autofocus placeholder="Konfirmasi Password baru anda disini">
+                                <input type="password" class="form-control" name="confirm_new_password" required autofocus placeholder="Konfirmasi Password baru anda disini">
                             </div>
                             <input type="submit" name="submit" value="Reset Password">
                         </form>
